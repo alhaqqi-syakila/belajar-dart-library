85374adab9e970d023d60354288c5330a02f3fb6
alhaqqi syakila <alhaqqisyakil2020@gmail.com>
2023-11-14 08:24:59 +0700

    commit 1

---
+++ example/belajar_dart_packages_library_self_example.dart
@@ -0,0 +1,6 @@
+import 'package:belajar_dart_packages_library_self/belajar_dart_packages_library_self.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_packages_library_self/hello.dart';
+
+void main(){
+  print(sayHello("Haqqi"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
